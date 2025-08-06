56589fc9aad69be1e2c1d34b77954321a9178436
lgm <2663253979@qq.com>
2025-08-06 16:13:03 +0800

    添加了代码生成的功能

--- src/lib/blocks.js
+++ src/lib/blocks.js
@@ -1,5 +1,9 @@
 import LazyScratchBlocks from './tw-lazy-scratch-blocks';
+import 'scratch-blocks/blockly_compressed_vertical'
+import 'scratch-blocks/blockly_compressed_horizontal'
+// import 'scratch-blocks/lua_compressed'
 
+import 'scratch-blocks/python_compressed'
 /**
  * Connect scratch blocks with the vm
  * @param {VirtualMachine} vm - The scratch vm
