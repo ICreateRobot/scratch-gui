57313e352209d56e9069c412c7a506f02818293f
lgm <2663253979@qq.com>
2026-01-07 14:19:29 +0800

    新增了波兰语和俄语的选项

--- src/components/menu-bar/language-menu.jsx
+++ src/components/menu-bar/language-menu.jsx
@@ -93,7 +93,7 @@ class LanguageMenu extends React.PureComponent {
                 >
                     {
                         Object.keys(locales)
-                            .filter(locale => ['en', 'zh-cn'].includes(locale))
+                            .filter(locale => ['en', 'zh-cn','pl','ru'].includes(locale))
                             .map(locale => (
                                 <MenuItem
                                     key={locale}
